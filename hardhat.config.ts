b13cf1fef95f3ffa85e81fae66e269f72036d36a
gnmaxim <maximgaina@yandex.ru>
2022-08-09 20:50:41 +0200

    chore: initial config

---
+++ hardhat.config.ts
@@ -0,0 +1,65 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+import * as dotenv from 'dotenv';
+
+// Tasks
+// import './tasks/deploy';
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.8.15",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+
+    paths: {
+        sources: './contracts',
+        tests: './test',
+        cache: './cache',
+        artifacts: './artifacts',
+    },
+
+    networks: {
+        hardhat: {},
+
+        goerli: {
+            url: process.env.DEV_GOERLI_RPC || '',
+            accounts: process.env.DEV_EOA_PRIV
+                ? [process.env.DEV_EOA_PRIV]
+                : [],
+            gasPrice: 10000000000,
+        },
+
+        rinkeby: {
+            url: process.env.DEV_RINK_RPC || '',
+            accounts: process.env.DEV_EOA_PRIV
+                ? [process.env.DEV_EOA_PRIV]
+                : [],
+            gasPrice: 5000000000,
+        },
+
+        // ethereum: {},
+    },
+
+    etherscan: {
+        apiKey: {
+            goerli: process.env.ETHERSCAN_API_KEY
+                ? process.env.ETHERSCAN_API_KEY
+                : '',
+        },
+    },
+
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: 'USD',
+    },
+};
+
+export default config;
